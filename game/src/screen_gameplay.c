58d9760758295640c5fa6fd47c518486d63f9dbe
{XenoBenn} <{benjamin.komar@gmail.com}>
2024-02-22 11:09:11 +0100

    updating player character to capsule

--- game/src/screen_gameplay.c
+++ game/src/screen_gameplay.c
@@ -50,19 +50,19 @@ void UpdateGameplayScreen(void)
     // TODO: Update GAMEPLAY screen variables here!
 
     // Press enter or tap to change to ENDING screen
-    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    if (IsKeyPressed(KEY_ENTER))
     {
         finishScreen = 1;
-        PlaySound(fxCoin);
     }
 }
-    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+
+    Vector3 capsulPosition = { 0.0f, 0.0f, 0.0f };
 
     // Define the camera to look into our 3d world
     Camera3D camera = {
 
     .position = (Vector3){ 0.0f, 10.0f, 10.0f },  // Camera position
-    .target = (Vector3){0.0f, 0.0f, 0.0f },      // Camera looking at point
+    .target = (Vector3){0.0f, 0.0f, 0.0f },       // Camera looking at point
     .up = (Vector3){ 0.0f, 1.0f, 0.0f },          // Camera up vector (rotation towards target)
     .fovy = 45.0f,                                // Camera field-of-view Y
     .projection = CAMERA_PERSPECTIVE              // Camera mode type
@@ -74,29 +74,33 @@ void DrawGameplayScreen(void)
 {
     // TODO: Draw GAMEPLAY screen here!
 
-        Vector3 cameraOffset = { 0.0f, 10.0f, 10.0f };
-        camera.position.x = cubePosition.x + cameraOffset.x;
-        camera.position.z = cubePosition.z + cameraOffset.z;
-
-    if (IsKeyDown(KEY_W)) {
-        cubePosition.z -= 1.0f;
-}
-    if (IsKeyDown(KEY_S)) {
-        cubePosition.z += 1.0f;
-}
-    if (IsKeyDown(KEY_A)) {
-        cubePosition.x -= 1.0f;
-}
-    if (IsKeyDown(KEY_D)) {
-        cubePosition.x += 1.0f;
-}
     BeginMode3D(camera);
-        DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-        DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+        DrawCapsule(capsulPosition, (Vector3){0.0f, 2.0f, 0.0f }, 2.0f, 8, 4, RED);
+        DrawCapsuleWires(capsulPosition, (Vector3){0.0f, 2.0f, 0.0f }, 2.0f, 6, 4, MAROON);
 
         DrawGrid(10, 1.0f);
     EndMode3D();
+
+    camera.target = capsulPosition;
+
+    if (IsKeyDown(KEY_W)) {
+        capsulPosition.z -= 0.1f;
 }
+    if (IsKeyDown(KEY_S)) {
+        capsulPosition.z += 0.1f;
+}
+    if (IsKeyDown(KEY_A)) {
+        capsulPosition.x -= 0.1f;
+}
+    if (IsKeyDown(KEY_D)) {
+        capsulPosition.x += 0.1f;
+}
+    UpdateCamera(&camera, CAMERA_THIRD_PERSON );
+    Vector3 cameraOffset = { 0.0f, 10.0f, 10.0f };
+    camera.position.x = capsulPosition.x + cameraOffset.x;
+    camera.position.z = capsulPosition.z + cameraOffset.z;
+}
+
 // Gameplay Screen Unload logic
 void UnloadGameplayScreen(void)
 {
