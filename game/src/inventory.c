b9ccedbb016f0b3a3ac619d42f1a213b0ff259e0
XenoBenn <benjamin.komar@gmail.com>
2024-02-28 22:30:15 +0100

    setting up the for loop to fill up the stash slots with rectangles

--- game/src/inventory.c
+++ game/src/inventory.c
@@ -3,6 +3,7 @@
 #include "inventory.h"
 #include <stdio.h>
 
+// Max rows and cols in the inventory stash that the player can have
 #define MAX_STASH_COLS 10
 #define MAX_STASH_ROWS 6
 //----------------------------------------------------------------------------------
@@ -18,6 +19,8 @@ Rectangle inventoryItemSlot;
 Rectangle inventoryItemBrowser;
 Rectangle inventoryBrowserBar;
 Rectangle inventoryStash;
+float itemSlotOffset = 8; // The needed amount to shift the rectangles to be offsetted
+Rectangle itemSlotRow[MAX_STASH_COLS];
 
 //----------------------------------------------------------------------------------
 // Inventory Functions Definition
@@ -25,11 +28,11 @@ Rectangle inventoryStash;
 void InitInventory() {
     Vector2 windowAnchor = { (float)screenWidth/2.0f - windowInventoryPos.width/2.0f, (float)screenHeight/2.0f - windowInventoryPos.height/2.0f };
     windowInventoryPos = (Rectangle){ windowAnchor.x, windowAnchor.y, 1016, 632 };
-    printf("Inventory window: %f x | %f y | %f width | %f height \n", windowInventoryPos.x, windowInventoryPos.y, windowInventoryPos.width, windowInventoryPos.height);
     inventoryStashBar = (Rectangle){ windowAnchor.x + 8, windowAnchor.y + 32, 656, 592 };
-    printf("Inventory stash bar: %f x | %f y | %f width | %f height \n", inventoryStashBar.x, inventoryStashBar.y, inventoryStashBar.width, inventoryStashBar.height);
-    inventoryItemSlot = (Rectangle){ windowAnchor.x + 24, windowAnchor.y + 48, 48, 48 };
-    printf("Inventory item slot: %f x | %f y | %f width | %f height \n", inventoryItemSlot.x, inventoryItemSlot.y, inventoryItemSlot.width, inventoryItemSlot.height);
+    // Iterating through an array to fill up the inventory stash with rectangles
+    for (int i = 0; i <= MAX_STASH_COLS; i++){
+        itemSlotRow[i] = (Rectangle){ windowAnchor.x + 24 + itemSlotOffset, windowAnchor.y + 48, 48, 48 };
+    }
     inventoryItemBrowser = (Rectangle){ windowAnchor.x + 672, windowAnchor.y + 32, 336, 592 };
     inventoryBrowserBar = (Rectangle){ windowAnchor.x + 680, windowAnchor.y + 40, 320, 384 };
     inventoryStash = (Rectangle){ windowAnchor.x + 16, windowAnchor.y + 40, 640, 384 };
@@ -42,7 +45,7 @@ void DrawInventory() {
         GuiPanel((Rectangle)inventoryItemBrowser, NULL); // Item bowser bar
         GuiPanel((Rectangle)inventoryBrowserBar, NULL); // Item bowser
         GuiPanel((Rectangle)inventoryStash, NULL); // Stash space
-        GuiPanel((Rectangle)inventoryItemSlot, NULL); // Item slot
+        GuiPanel((Rectangle)*itemSlotRow, NULL); // Item slot
     }
 }
 
